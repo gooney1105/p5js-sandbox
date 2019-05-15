5af13b5aba3e2620ce5f98bacf2da64cc8a719ae
YUMI PARK <yumipark@YUMIs-MacBook-Pro.local>
2019-05-15 09:52:12 +0900

    face drawing

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -6,4 +6,14 @@ function setup() {
 function draw() {
     console.log("draw");
     background('#00bb88');
+    fill(255, 204, 0);
+    circle(400, 300, 300);
+    fill('white');
+    circle(400 - 70, 300 - 50, 100);
+    circle(400 + 70, 300 - 50, 100);
+    fill('black');
+    circle(400 - 70, 300 - 50, 50);
+    circle(400 + 70, 300 - 50, 50);
+    fill('red');
+    arc(400, 350, 150, 100, 0, PI, CHORD);
 }
