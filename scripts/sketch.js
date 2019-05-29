cdc4c7054074cf536f65efc5aa81981a5db97e50
YUMI PARK <yumipark@YUMIs-MacBook-Pro.local>
2019-05-29 23:12:12 +0900

    face detailed drawing

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -5,15 +5,38 @@ function setup() {
 
 function draw() {
     console.log("draw");
+    const faceX = width / 2;
+    const faceY = height / 2;
+    const faceRadius = 150;
     background('#00bb88');
     fill(255, 204, 0);
-    circle(400, 300, 300);
+    circle(faceX, faceY, faceRadius * 2);
+
+    // 흰자 그리기
     fill('white');
-    circle(400 - 70, 300 - 50, 100);
-    circle(400 + 70, 300 - 50, 100);
+    const leftEyeX = faceX - 70;
+    const leftEyeY = faceY - 50;
+    const rightEyeX = faceX + 70;
+    const rightEyeY = faceY - 50;
+    const eyeRadius = 50;
+    
+    // 왼쪽 흰자
+    circle(leftEyeX, leftEyeY, eyeRadius * 2);
+    // 오른쪽 흰자
+    circle(faceX + 70, faceY - 50, eyeRadius * 2);
+
+    // 눈동자 그리기
     fill('black');
-    circle(400 - 70, 300 - 50, 50);
-    circle(400 + 70, 300 - 50, 50);
+    const pupilRadius = 25;
+    const leftPupilX = leftEyeX - (eyeRadius - pupilRadius);
+    const rightPupilX = rightEyeX - (eyeRadius - pupilRadius);
+
+    // 왼쪽 눈동자
+    circle(leftPupilX, leftEyeY, pupilRadius * 2);
+    // 오른쪽 눈동자
+    circle(rightPupilX, faceY - 50, pupilRadius * 2);
+
+    // 입 그리기
     fill('red');
-    arc(400, 350, 150, 100, 0, PI, CHORD);
+    arc(faceX, faceY + 50, 150, 100, 0, PI, CHORD);
 }
