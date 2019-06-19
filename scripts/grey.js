0be831ef3e013ed52586997d03ce85bb1096e4a6
YUMI PARK <yumipark@YUMIs-MacBook-Pro.local>
2019-06-19 10:01:50 +0900

    draw image center

--- scripts/grey.js
+++ scripts/grey.js
@@ -1,7 +1,20 @@
+let img;
+
+function preload() {
+    img = loadImage('https://avatars1.githubusercontent.com/u/50386683?s=460&v=4');
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }   
 
 function draw() {
     background('#FE6390');
+
+    const centerY = height/2;
+    const imageX = (width - img.width) / 2;
+    const imageY = centerY - img.height/2;
+    image(img, imageX, imageY);
+
+    console.log(img.width, img.height);    
 }
